a695d9af8358b3b4ea9dee6a2b6181f5e15b4ecb
Gerald Mir <geramir91@gmail.com>
2024-03-30 20:25:22 +0300

    add task 2 exam 5

--- exam_chapter5.py
+++ exam_chapter5.py
@@ -1,5 +1,25 @@
-#Задача 1
-a = int(input())
-if a%100 == 0:
+# Задача 1
+# a = int(input())
+# if a%100 == 0:
+#     print('YES')
+# else: print('NO')
+
+#Задача 2
+a1,a2,b1,b2= int(input()),int(input()),int(input()),int(input())
+if a1%2 !=0:
+    if a2%2 !=0:
+        color1=1 # белая клетка
+    else: color1=2 # черная клетка
+elif a2%2 !=0:
+    color1=2
+else: color1=1
+if b1%2 !=0:
+    if b2%2 !=0:
+        color2=1 # белая клетка
+    else: color2=2 # черная клетка
+elif b2%2 !=0:
+    color2=2
+else: color2=1
+if color1 == color2:
     print('YES')
 else: print('NO')
