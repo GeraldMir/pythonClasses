e3146971bd6de975f92b69943ffe40355abff8fc
Gerald Mir <geramir91@gmail.com>
2024-03-30 20:46:32 +0300

    add task 4 exam 5

--- exam_chapter5.py
+++ exam_chapter5.py
@@ -25,7 +25,32 @@
 # else: print('NO')
 
 # Задача 3
-old, sex = int(input()), input()
-if 10<=old<=15 and sex == 'f':
-    print('YES')
-else: print('NO')
+# old, sex = int(input()), input()
+# if 10<=old<=15 and sex == 'f':
+#     print('YES')
+# else: print('NO')
+
+# Задача 4
+a=int(input())
+if 1<=a<=10:
+    if a ==1:
+        print('I')
+    elif a==2:
+        print('II')
+    elif a==3:
+        print('III')
+    elif a==4:
+        print('IV')
+    elif a==5:
+        print('V')
+    elif a==6:
+        print('VI')
+    elif a==7:
+        print('VII')
+    elif a==8:
+        print('VIII')
+    elif a==9:
+        print('IX')
+    elif a==10:
+        print('X')
+else: print('ошибка')
