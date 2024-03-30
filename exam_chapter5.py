4f50e4ac79c4b932c89b18db6b18375434aefacb
Gerald Mir <geramir91@gmail.com>
2024-03-30 20:30:32 +0300

    add task 3 exam 5

--- exam_chapter5.py
+++ exam_chapter5.py
@@ -4,22 +4,28 @@
 #     print('YES')
 # else: print('NO')
 
-#Задача 2
-a1,a2,b1,b2= int(input()),int(input()),int(input()),int(input())
-if a1%2 !=0:
-    if a2%2 !=0:
-        color1=1 # белая клетка
-    else: color1=2 # черная клетка
-elif a2%2 !=0:
-    color1=2
-else: color1=1
-if b1%2 !=0:
-    if b2%2 !=0:
-        color2=1 # белая клетка
-    else: color2=2 # черная клетка
-elif b2%2 !=0:
-    color2=2
-else: color2=1
-if color1 == color2:
+# Задача 2
+# a1,a2,b1,b2= int(input()),int(input()),int(input()),int(input())
+# if a1%2 !=0:
+#     if a2%2 !=0:
+#         color1=1 # белая клетка
+#     else: color1=2 # черная клетка
+# elif a2%2 !=0:
+#     color1=2
+# else: color1=1
+# if b1%2 !=0:
+#     if b2%2 !=0:
+#         color2=1 # белая клетка
+#     else: color2=2 # черная клетка
+# elif b2%2 !=0:
+#     color2=2
+# else: color2=1
+# if color1 == color2:
+#     print('YES')
+# else: print('NO')
+
+# Задача 3
+old, sex = int(input()), input()
+if 10<=old<=15 and sex == 'f':
     print('YES')
 else: print('NO')
