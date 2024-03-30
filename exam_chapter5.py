e469ef0fe07bc9e2d8e61db9845018f85f215422
Gerald Mir <geramir91@gmail.com>
2024-03-30 20:52:40 +0300

    add task 5 exam 5

--- exam_chapter5.py
+++ exam_chapter5.py
@@ -31,26 +31,37 @@
 # else: print('NO')
 
 # Задача 4
-a=int(input())
-if 1<=a<=10:
-    if a ==1:
-        print('I')
-    elif a==2:
-        print('II')
-    elif a==3:
-        print('III')
-    elif a==4:
-        print('IV')
-    elif a==5:
-        print('V')
-    elif a==6:
-        print('VI')
-    elif a==7:
-        print('VII')
-    elif a==8:
-        print('VIII')
-    elif a==9:
-        print('IX')
-    elif a==10:
-        print('X')
-else: print('ошибка')
+# a=int(input())
+# if 1<=a<=10:
+#     if a ==1:
+#         print('I')
+#     elif a==2:
+#         print('II')
+#     elif a==3:
+#         print('III')
+#     elif a==4:
+#         print('IV')
+#     elif a==5:
+#         print('V')
+#     elif a==6:
+#         print('VI')
+#     elif a==7:
+#         print('VII')
+#     elif a==8:
+#         print('VIII')
+#     elif a==9:
+#         print('IX')
+#     elif a==10:
+#         print('X')
+# else: print('ошибка')
+
+# Задача 5
+a = int(input())
+if a%2 !=0:
+    print("YES")
+elif 2<=a<=5:
+    print('NO')
+elif 6<=a<=20:
+    print('YES')
+elif a>20:
+    print('NO')
