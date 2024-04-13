b58f441a65dde67fd2ed94f7b50f06573e9cb6de
gerald <geramir91@gmail.com>
2024-04-13 21:17:09 +0400

    add task 3 chapter 6

--- chapte6.py
+++ chapte6.py
@@ -3,5 +3,9 @@
 # print(0.5 * a * b)
 
 # task 2
-s, v1, v2 = float(input()), float(input()), float(input())
-print(s/(v1+v2))
+# s, v1, v2 = float(input()), float(input()), float(input())
+# print(s/(v1+v2))
+
+# task 3
+n = float(input())
+print(1/n if n != 0 else 'Обратного числа не существует')
