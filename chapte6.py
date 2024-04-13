076d533ac0bc496412393974e610c6cbb7be1b83
gerald <geramir91@gmail.com>
2024-04-13 21:11:06 +0400

    add task 2 chapter 6

--- chapte6.py
+++ chapte6.py
@@ -1,3 +1,7 @@
 # task 1
-a, b = float(input()), float(input())
-print(0.5 * a * b)
+# a, b = float(input()), float(input())
+# print(0.5 * a * b)
+
+# task 2
+s, v1, v2 = float(input()), float(input()), float(input())
+print(s/(v1+v2))
