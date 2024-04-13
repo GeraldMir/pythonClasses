4d66007131776d275b3e6eff2a156c1131b04ba5
gerald <geramir91@gmail.com>
2024-04-13 21:33:57 +0400

    add task 4 and task 5 chapter 6

--- chapte6.py
+++ chapte6.py
@@ -7,5 +7,13 @@
 # print(s/(v1+v2))
 
 # task 3
-n = float(input())
-print(1/n if n != 0 else 'Обратного числа не существует')
+# n = float(input())
+# print(1/n if n != 0 else 'Обратного числа не существует')
+
+# task 4
+# f = float(input())
+# print((5/9)*(f-32))
+
+# task 5
+n=float(input())
+print(n*10.5 if 0<=n<=2 else 4*(n-2)+21)
