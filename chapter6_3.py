a1f4dc32bfcaa1d702cf1310d9e6cb104e6a07fb
Gerald Mir <geramir91@gmail.com>
2024-04-29 16:53:57 +0300

    add task 4 for chapter 6.3

--- chapter6_3.py
+++ chapter6_3.py
@@ -8,6 +8,12 @@
 # print(math.pi*r**2, 2*math.pi*r, sep='\n', end='')
 
 # task 3
-a,b = float(input()),float(input())
-print((a+b)/2, (a*b)**0.5, 2*a*b/(a+b), ((a**2+b**2)/2)**0.5, 
-      sep='\n')
+# a,b = float(input()),float(input())
+# print((a+b)/2, (a*b)**0.5, 2*a*b/(a+b), ((a**2+b**2)/2)**0.5, 
+#       sep='\n')
+
+# task 4
+x = float(input())
+import math
+r = math.radians(x)
+print(math.sin(r)+math.cos(r)+math.pow(math.tan(r),2))
