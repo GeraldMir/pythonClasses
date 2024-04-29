e6ce2fc393c1cd5e28b2f7b528c7f26d1875184d
Gerald Mir <geramir91@gmail.com>
2024-04-29 16:44:51 +0300

    add tasks 2, 3 for chapter 6.3

--- chapter6_3.py
+++ chapter6_3.py
@@ -1,3 +1,13 @@
 # task 1
-x1,y1,x2,y2 = float(input()),float(input()),float(input()),float(input())
-print(((x1-x2)**2+(y1-y2)**2)**0.5)
+# x1,y1,x2,y2 = float(input()),float(input()),float(input()),float(input())
+# print(((x1-x2)**2+(y1-y2)**2)**0.5)
+
+#task 2
+# r = float(input())
+# import math
+# print(math.pi*r**2, 2*math.pi*r, sep='\n', end='')
+
+# task 3
+a,b = float(input()),float(input())
+print((a+b)/2, (a*b)**0.5, 2*a*b/(a+b), ((a**2+b**2)/2)**0.5, 
+      sep='\n')
