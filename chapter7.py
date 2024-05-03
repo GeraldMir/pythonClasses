465a3c7d068d297632ca365250cace8ee1d13b03
Gerald Mir <geramir91@gmail.com>
2024-05-04 01:16:58 +0300

    add some tasks for chapter 7 (4,5,6) and I drink whiskey Ho-hoho

--- chapter7.py
+++ chapter7.py
@@ -7,9 +7,21 @@
 # print(asimpt-math.log(n))
 
 # task 4
-n = int(input())
-summ=0
-for i in range(1,n+1):
-    if (i**2)%10 ==2 or (i**2)%10 == 5 or (i**2)%10 == 8:
-        summ +=i
-print(summ)
+# n = int(input())
+# summ=0
+# for i in range(1,n+1):
+#     if (i**2)%10 ==2 or (i**2)%10 == 5 or (i**2)%10 == 8:
+#         summ +=i
+# print(summ)
+
+# task 5
+# import math
+# print(math.factorial(int(input())))
+
+# task 6
+total = 1
+for i in range(10):
+    n = int(input())
+    if n != 0:
+        total *= n
+print(total)
